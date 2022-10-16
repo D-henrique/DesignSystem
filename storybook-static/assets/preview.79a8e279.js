9687a5ef3aab0a54cca2154d77677c7e1f19cc55
Douglas Moreira <d.ouglashenrique@hotmail.com>
2022-10-16 14:15:38 -0300

    fixed gitignore issue

--- storybook-static/assets/preview.79a8e279.js
+++
@@ -1,2 +0,0 @@
-import{t as o}from"./index.cf857b0c.js";import"./iframe.def1683a.js";import"./es.map.constructor.944c994b.js";import"./es.number.to-fixed.cc7e6d4d.js";import"./index.5257e58c.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
-//# sourceMappingURL=preview.79a8e279.js.map
